e2bbf19e5221fe3c66c9b702493676e5fc90a1e5
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2023-05-06 10:53:09 +0900

    add: 풀이 주석 추가

--- greedy/10162전자레인지.py
+++ greedy/10162전자레인지.py
@@ -6,7 +6,7 @@ for btn in btns:
     counts.append(t // btn)
     t = t % btn
 
-if t != 0: # 나머지가 0이 아닐 때 -3
+if t != 0: # 나머지가 0이 아닐 때 -1
     print(-1)
 else: # 나머지가 0 일 때 count 차례로 출력
     for count in counts:
--- greedy/2217로프.py
+++ greedy/2217로프.py
@@ -1,13 +1,27 @@
-n = int(input())
+"""
+알고리즘 : 
+그리디, 정렬
+
+풀이 : 
+정렬을 하여 각 위치 만큼 값 최대로 나올 수 있는 경우 찾아내기
+
+예시 :
+ropes = [26, 13, 11, 20]
+reverse = [26, 20, 13, 11]
+tot_weight = [26*1 = 26, 20*2 = 40, 13*3 = 39, 11*4 = 44 ]
+"""
+
+
+n = int(input()) # 로프 종류 갯수
 ropes = []
 
 for i in range(n):
-    ropes.append(int(input()))
+    ropes.append(int(input())) # 로프 종류 받기
     
-ropes.sort(reverse=True)
+ropes.sort(reverse=True) # 로프 큰 순으로 역순으로 정렬
 result = []
 
 for j in range(n):
-    result.append(ropes[j] * (j+1))
+    result.append(ropes[j] * (j+1)) # 로프 종류 만큼 최대로 버틸 수 있는 무게
     
 print(max(result))
